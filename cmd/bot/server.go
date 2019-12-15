0186e4ee3e83f4fa82d89c63fc7125999ebfe1d5
wolf1996 <ksgiv37@gmail.com>
2019-12-15 19:21:10 +0300

    Graceful shutdown with context

--- cmd/bot/server.go
+++ cmd/bot/server.go
@@ -5,11 +5,14 @@ import (
 	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"flag"
+	"net/http"
+
 	bot "github.com/wolf1996/HandWitch/pkg/bot"
 	"github.com/wolf1996/HandWitch/pkg/core"
-	"net/http"
 )
 
 func getDescriptionSourceFromFile(path string) (*core.URLProcessor, error) {
@@ -47,13 +50,20 @@ func main() {
 	whiteListPath := flag.String("whitelist", "", "path to list of allowed users")
 	flag.Parse()
 	var err error
+	// из-за блокировок телеграмм даём возможность работы через прокси
 	client := http.DefaultClient
 	if *proxy != "" {
 		log.Printf("Got Proxy %s", *proxy)
 		client, err = bot.GetClientWithProxy(*proxy)
+		if err != nil {
+			log.Fatalf("Failed to create http client with proxy %s", err.Error())
+		}
 	} else {
 		log.Print("Got No Proxy")
 	}
+
+	// пытаемся собрать список разрешённых пользователей
+	// если такой список не указан - доступны всем ветрам
 	var auth bot.Authorisation
 	if *whiteListPath != "" {
 		auth, err = getAuthSourceFromFile(*whiteListPath)
@@ -64,20 +74,38 @@ func main() {
 		auth = bot.DummyAuthorisation{}
 		log.Printf("No whitelist found starting with dummy auth")
 	}
-	if err != nil {
-		log.Fatalf("Failed %s", err.Error())
-	}
+
+	// грузим описания ручек
 	log.Printf("Description file path used %s", *path)
 	urlContainer, err := getDescriptionSourceFromFile(*path)
 	if err != nil {
 		log.Fatalf("Failed to get description source file %s", err.Error())
 	}
+
+	// собираем бота
 	botInstance, err := bot.NewBot(client, *token, *urlContainer, auth)
 	if err != nil {
 		log.Fatalf("Failed tto create bot %s", err.Error())
 	}
-	err = botInstance.Listen(context.Background())
+
+	// Вешаем обработчики сигналов на контекст
+	ctx, cancel := context.WithCancel(context.Background())
+	sysSignals := make(chan os.Signal, 1)
+
+	signal.Notify(sysSignals,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	go func() {
+		signal := <-sysSignals
+		log.Printf("Got %s system signal, aborting...", signal)
+		cancel()
+	}()
+
+	err = botInstance.Listen(ctx)
 	if err != nil {
-		log.Fatalf("Failed on listen %s", err.Error())
+		log.Fatalf("Stopping bot %s", err.Error())
 	}
 }
