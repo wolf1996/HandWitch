2c409e95c5ef9885224b36c3ddab9dc16a9bc4ec
wolf1996 <ksgiv37@gmail.com>
2020-01-03 01:07:15 +0300

    переходный вариант

--- cmd/servetg.go
+++ cmd/servetg.go
@@ -1,18 +1,28 @@
 package cmd
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	bot "github.com/wolf1996/HandWitch/pkg/bot"
 )
 
 func exec(cmd *cobra.Command, args []string) {
+
 	loglevelStr := viper.GetString("log")
 	loglevel, err := log.ParseLevel(loglevelStr)
 	if err != nil {
 		log.Fatalf("Failed to parse LogLevel %s", err.Error())
 	}
 	logger := log.StandardLogger()
+	logger.Infof("Used config: %s", viper.ConfigFileUsed())
+
 	logger.SetLevel(loglevel)
 
 	path := viper.GetString("path")
@@ -27,7 +37,67 @@ func exec(cmd *cobra.Command, args []string) {
 	tgproxy := viper.GetString("telegram.proxy")
 	logger.Infof("Used proxy for telegram client: %s", tgproxy)
 
-	logger.Infof("Used config: %s", viper.ConfigFileUsed())
+	token := cmd.Flags().Lookup("token").Value.String()
+
+	client := http.DefaultClient
+	if tgproxy != "" {
+		log.Infof("Got Proxy %s", tgproxy)
+		client, err = bot.GetClientWithProxy(tgproxy)
+		if err != nil {
+			log.Fatalf("Failed to create http client with proxy %s", err.Error())
+		}
+	} else {
+		log.Info("Got No Proxy")
+	}
+
+	// пытаемся собрать список разрешённых пользователей
+	// если такой список не указан - доступны всем ветрам
+	var auth bot.Authorisation
+	if whitelist != "" {
+		auth, err = getAuthSourceFromFile(whitelist)
+		if err != nil {
+			log.Fatalf("Failed to get auth %s, stop", err.Error())
+		}
+	} else {
+		auth = bot.DummyAuthorisation{}
+		log.Info("No whitelist found starting with dummy auth")
+	}
+
+	// грузим описания ручек
+	log.Infof("Description file path used %s", path)
+	urlContainer, err := getDescriptionSourceFromFile(path)
+	if err != nil {
+		log.Fatalf("Failed to get description source file %s", err.Error())
+	}
+
+	log.Info("Creating telegram bot api client")
+	botInstance, err := bot.NewBot(client, token, *urlContainer, auth, formating)
+	if err != nil {
+		log.Fatalf("Failed to create bot %s", err.Error())
+	}
+
+	log.Info("Telegram bot api client created")
+
+	// Вешаем обработчики сигналов на контекст
+	ctx, cancel := context.WithCancel(context.Background())
+	sysSignals := make(chan os.Signal, 1)
+
+	signal.Notify(sysSignals,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	go func() {
+		signal := <-sysSignals
+		log.Infof("Got %s system signal, aborting...", signal)
+		cancel()
+	}()
+
+	err = botInstance.Listen(ctx, logger)
+	if err != nil {
+		log.Infof("Stopping bot %s", err.Error())
+	}
 }
 
 func registerServeBot(parentCmd *cobra.Command) {
