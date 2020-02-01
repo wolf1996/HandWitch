34bf18faea162b948be72152f1bf54b864199516
wolf1996 <ksgiv37@gmail.com>
2020-02-02 00:20:11 +0300

    Поддержать новую логику

--- pkg/core/descriptors.go
+++ pkg/core/descriptors.go
@@ -230,7 +230,7 @@ func (p *ParamProcessorImp) GetInfo() ParamInfo {
 
 //IsRequired check if parameter is required
 func (p *ParamProcessorImp) IsRequired() bool {
-	return p.Destination == URLPlaced
+	return !p.Optional
 }
 
 //ParseFromString get param value from string
